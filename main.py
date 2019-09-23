fab28ca71f60057a4e345607d38b2f801cf24cdb
Ray Vector <rayvectorspqr@yahoo.com>
2019-09-23 18:02:31 +0300

    done, first test + new data

--- main.py
+++ main.py
@@ -1,40 +1,126 @@
 import numpy as np
-
-music = 1.0
-rain = 1.0
-girls = 0.0
+import sys
 
 
-random_number = np.around(np.random.uniform(low=0, high=1, size=3), decimals=2)
-for z in random_number:
-    print(z)
+class PartyNN(object):
+    def __init__(self, learning_rate=0.1):
+        self.weights_0_1 = np.random.normal(0.0, 2 ** -0.5, (2, 3))
+        self.weights_1_2 = np.random.normal(0.0, 1, (1, 2))
+        self.sigmoid_mapper = np.vectorize(self.sigmoid)
+        self.learning_rate = np.array([learning_rate])
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def predict(self, inputs):
+        inputs_1 = np.dot(self.weights_0_1, inputs)
+        outputs_1 = self.sigmoid_mapper(inputs_1)
+
+        inputs_2 = np.dot(self.weights_1_2, outputs_1)
+        outputs_2 = self.sigmoid_mapper(inputs_2)
+        return outputs_2
+
+    def training(self, inputs, expected_predict):
+        inputs_1 = np.dot(self.weights_0_1, inputs)
+        outputs_1 = self.sigmoid_mapper(inputs_1)
+
+        inputs_2 = np.dot(self.weights_1_2, outputs_1)
+        outputs_2 = self.sigmoid_mapper(inputs_2)
+        actual_predict = outputs_2[0]
+
+        error_layer_2 = np.array([actual_predict - expected_predict])
+        gradient_layer_2 = actual_predict * (1 - actual_predict)
+        weights_delta_layer_2 = error_layer_2 * gradient_layer_2
+        self.weights_1_2 -= (np.dot(weights_delta_layer_2, outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
+
+        error_layer_1 = weights_delta_layer_2 * self.weights_1_2
+        gradient_layer_1 = outputs_1 * (1 - outputs_1)
+        weights_delta_layer_1 = error_layer_1 * gradient_layer_1
+        self.weights_0_1 -= (np.dot(inputs.reshape(len(inputs), 1), weights_delta_layer_1).T * self.learning_rate)
 
 
-def sig(x):
-    return np.around(1 / (1 + np.exp(-x)), decimals=2)
+def mse(y, Y):
+    return np.mean((y - Y) ** 2)
+
+train = [
+    ([0, 0, 0], 0),
+    ([1, 1, 1], 1),
+    ([1, 0, 1], 1),
+    ([1, 1, 1], 1),
+    ([1, 0, 0], 1),
+]
 
 
-def predict(a, b, c):
-    inputs = np.array([a, b, c])
-    weights_hidden1 = np.around(np.random.uniform(low=0, high=1, size=3), decimals=2)
-    weights_hidden2 = np.around(np.random.uniform(low=0, high=1, size=3), decimals=2)
-    weights_hidden = np.array([weights_hidden1, weights_hidden2])
+epochs = 9000
+learning_rate = 0.7
 
-    weights_hidden_to_output = np.array([0.5, 0.52])
+network = PartyNN(learning_rate=learning_rate)
 
-    hidden_input = np.dot(weights_hidden, inputs)
-    print('hidden_input: ' + str(hidden_input))
+for e in range(epochs):
+    inputs_ = []
+    correct_predictions = []
+    for input_stat, correct_predict in train:
+        network.training(np.array(input_stat), correct_predict)
+        inputs_.append(np.array(input_stat))
+        correct_predictions.append(np.array(correct_predict))
 
-    hidden_output = np.array([sig(x) for x in hidden_input])
-    print('hidden_output: ' + str(hidden_output))
+    train_loss = mse(network.predict(np.array(inputs_).T), np.array(correct_predictions))
+    sys.stdout.write("\rProgress: {}, Training loss: {}".format(str(100 * e / float(epochs))[:4], str(train_loss)[:5]))
 
-    output = np.dot(weights_hidden_to_output, hidden_output)
-    print('output: ' + str(output) + '\n')
+print("\n")
 
-    if sig(output) < 0.5:
-        return 'Yes'
-    else:
-        return 'No'
+for input_stat, correct_predict in train:
+    print("For input: {} the prediction is: {}, expected: {}".format(
+        str(input_stat),
+        str(int(network.predict(np.array(input_stat)) > .5)),
+        str(int(correct_predict == 1))
+    ))
+
+print("\n")
+
+for input_stat, correct_predict in train:
+    print("For input: {} the prediction is: {}, expected: {}".format(
+        str(input_stat),
+        str(network.predict(np.array(input_stat))),
+        str(int(correct_predict == 1))
+    ))
 
 
-print('result: ' + str(predict(music, rain, girls)))
+train2 = [
+    ([0, 0, 0], 0),
+    ([1, 1, 1], 1),
+    ([0, 0, 1], 0),
+    ([0, 1, 1], 0),
+    ([0, 0, 0], 0),
+]
+
+network2 = PartyNN(learning_rate=learning_rate)
+
+for e in range(epochs):
+    inputs_ = []
+    correct_predictions = []
+    for input_stat, correct_predict in train2:
+        network2.training(np.array(input_stat), correct_predict)
+        inputs_.append(np.array(input_stat))
+        correct_predictions.append(np.array(correct_predict))
+
+    train_loss = mse(network2.predict(np.array(inputs_).T), np.array(correct_predictions))
+    sys.stdout.write("\rProgress: {}, Training loss: {}".format(str(100 * e / float(epochs))[:4], str(train_loss)[:5]))
+
+print("\n")
+
+for input_stat, correct_predict in train2:
+    print("For input: {} the prediction is: {}, expected: {}".format(
+        str(input_stat),
+        str(int(network2.predict(np.array(input_stat)) > .5)),
+        str(int(correct_predict == 1))
+    ))
+
+print("\n")
+
+for input_stat, correct_predict in train2:
+    print("For input: {} the prediction is: {}, expected: {}".format(
+        str(input_stat),
+        str(network2.predict(np.array(input_stat))),
+        str(int(correct_predict == 1))
+    ))
